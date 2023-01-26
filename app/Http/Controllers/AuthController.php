ec57a59ac1bd298c14fbaee5e684b4ab92f13a61
Gerardo Rodríguez <gerdomx@gmail.com>
2023-01-25 20:06:04 -0600

    Comenatario

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -37,7 +37,7 @@ class AuthController extends Controller
   public function logout(Request $request)
   {
     $request->user()->currentAccessToken()->delete();
-    return response()->json(["status" => true, "message" => "bye"]);
+    return response()->json(["status" => true, "message" => "sesión cerrada exitosamente"]);
   }
 
   public function verificarAutenticacion(Request $request)
